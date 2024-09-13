c278bfeed98099aac9567dc8dfe6422216ebe83c
Alejo <alejoczombos@gmail.com>
2024-09-12 21:40:28 -0300

    Configuración para habilitar CORS

---
+++ src/main/java/com/example/tiny_url/infrastructure/rest/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.tiny_url.infrastructure.rest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+
+}
